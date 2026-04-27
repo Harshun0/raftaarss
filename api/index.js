5bf39ed7dbc0bafd9856dbe3cd45067747f141d4
Harsh Nebhnani <harshnebhnani02@gmail.com>
2026-04-28 04:08:31 +0530

    fix: create proper Vercel serverless function handler for TanStack Start

--- api/index.js
+++ api/index.js
@@ -1,3 +1,26 @@
 const server = require('../dist/server/server.js')
 
-module.exports = server.default
+module.exports = async (req, res) => {
+  try {
+    const response = await server.default.fetch(req.url, {
+      method: req.method,
+      headers: req.headers,
+      body: req.body
+    });
+    
+    res.status(response.status);
+    
+    response.headers.forEach((value, key) => {
+      res.setHeader(key, value);
+    });
+    
+    if (response.body) {
+      response.body.pipe(res);
+    } else {
+      res.end(await response.text());
+    }
+  } catch (error) {
+    console.error('Server function error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
